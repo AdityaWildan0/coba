58c606579dcf58a4be94a27c07fd78ed50d53930
11claraputri <116608746+11claraputri@users.noreply.github.com>
2022-10-25 14:04:29 +0800

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ventela</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body >
+    <nav class=" sticky top-0 shadow-md bg-white container-2xl mx-auto  text-center w-full font-mono shawdow-md">
+        <div class="flex flex-wrap items-center justify-between text-black text-[21px] mt-8 font-bold mr-[310px]">
+            <span class=" w-[190px] h-[61px] ml-[270px]">
+                <img src="https://static.wixstatic.com/media/d864c4_c51d8ecfe5a0469b9218a2e4afb1b4d6~mv2.jpg/v1/crop/x_224,y_0,w_709,h_191/fill/w_239,h_61,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/WARNA-BARU.jpg" alt="">
+            </span>
+            <div class="flex space-x-5">
+                <a class="hover:text-blue-600 transition duration-300 ease-in-out" href="">HOME</a>
+                <a class="hover:text-blue-600 transition duration-300 ease-in-out" href="">ABOUT</a>
+                <a class="hover:text-blue-600 transition duration-300 ease-in-out" href="">PRODUCTS</a>
+                <a class="hover:text-blue-600 transition duration-300 ease-in-out" href="">SIZE CHART</a>
+                <a class="hover:text-blue-600 transition duration-300 ease-in-out" href="">FAQ</a>
+                <a class="hover:text-blue-600 transition duration-300 ease-in-out" href="">CONTACT</a>
+            </div>
+        </div>
+    </nav>
+
+    <center>
+        
+    <div class="w-full bg-white">
+        <img src="https://static.wixstatic.com/media/d864c4_0660d796987e48ad83c7793bf987053f~mv2.jpg/v1/fill/w_1347,h_711,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/d864c4_0660d796987e48ad83c7793bf987053f~mv2.jpg" alt="">
+    </div>
+    </center>
